02da8d250e3901810da45c6194d21f9f9b9ad0ed
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-07-08 21:54:57 +0100

    Register Page

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { register } from '../redux/actions/authActions';
+
+const Register = ({ history }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const { userInfo, error } = useSelector((state) => state.auth);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(register(name, email, password));
+  };
+
+  if (userInfo) {
+    history.push('/profile');
+  }
+
+  return (
+    <div>
+      <h2>Register</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={submitHandler}>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
